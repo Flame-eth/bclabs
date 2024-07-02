48b2bfec253bdacc219bb1bd5db590f33623d9ac
Flame-eth <theflamepen@gmail.com>
2024-07-02 17:51:56 +0100

    feat: navbar UI

---
+++ src/constants/index.ts
@@ -0,0 +1,23 @@
+
+export const navbarHeaders: {
+  name: string;
+  link: string;
+}[] = [
+  {
+    name: "Exchange",
+    link: "/",
+  },
+  {
+    name: "Last Transactions",
+    link: "/",
+  },
+  {
+    name: "Invite Friend",
+    link: "/",
+  },
+  {
+    name: "Notifications",
+    link: "/",
+  },
+];
+
