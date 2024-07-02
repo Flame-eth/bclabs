9bee26ab056da4b71344f8ba0e38610e0bc3dfd7
Flame-eth <theflamepen@gmail.com>
2024-07-02 19:16:03 +0100

    feat: Add Assets and Hero components, update page layout
    This commit adds the Assets and Hero components to the page. It also updates the page layout by adding a gap between the flex items and changing the background image properties. This improves the overall design and user experience.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
+import Assets from "@/components/Assets";
+import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import React from "react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen h-[2000px] flex-col items-center justify-between bg-hero-bg bg-no-repeat no-scrollbar bg-top">
+    <main className="flex min-h-screen h-[2000px] flex-col gap-10 items-center  bg-hero-bg bg-no-repeat no-scrollbar bg-top bg-contain">
       <Navbar />
-      
+      <Hero />
+      <Assets />
     </main>
   );
 }
